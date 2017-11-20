43690a8f1abc31d98e02ccc5be50e3022dc45d94
Vladislav Slyusarev <vnvdvc@gmail.com>
2017-11-20 15:23:19 +0300

    code for experiments

---
+++ check_symmetry.py
@@ -0,0 +1,22 @@
+from phi import Phi
+from integrate import *
+
+def trim(list):
+    i = 0
+    while list[i] == 0:
+        i += 1
+    return list[i:]
+
+def is_symmetric(list):
+    n = len(list) // 2
+    for i in range(n):
+        if(list[i] != list[len(list) - i - 1]):
+            return False
+    return True
+for p in range(3, 100):
+    PHI=Phi(10, p).blocks
+    for m in range(1, 10):
+        list = integrate(PHI[m], p, mode='list')
+        if not is_symmetric(trim(list)):
+            print("False for p =", p, "m =", m)
+    print('checked for p = ', p)
