d056e0bf28c9cd26ed8b9e9a213c5fd2ccee5653
Vladislav Slyusarev <vnvdvc@gmail.com>
2017-12-12 22:17:45 +0300

    more omegas: linear omegas work, legendre omega works if p is not pythagorean prime number (otherwise false only for m=3 so far)

--- omegas.py
+++ omegas.py
@@ -1,4 +1,5 @@
 import legendre
+import random
 
 def omega_0(x, p):
     if x >= 0 and x < p - 1:
@@ -54,3 +55,36 @@ def legendre_omega(x, p):
     return legendre.calculateLegendre(x + 1, p) + 1
 # symmetric unless p is pythagorean prime
 # for pythagorean primes symmetric unless m=3
+
+
+#def generate_correct_omega(p, list):
+#    return lambda x: (list[x] if x < (p - 1) // 2 else 1 - list[p-2-x])
+
+def generate_binary_omega(p):
+    list = [random.randint(0,1) for j in range((p-1)//2 + (p-1)%2)]
+    return lambda x, p: (list[x] if x < (p - 1) // 2 else 1 - list[p-2-x])
+
+def generate_basic_omega(p):
+    values = random.shuffle([i for i in range((p-1)//2 + (p-1)%2)])
+    return lambda x, p: (list[x] if x < (p - 1) // 2 else p-2 - list[p-2-x])
+
+def generate_omega_for_th2(p):
+    zeta = random.randint(1,p-2)
+    list = [random.randint(0, zeta) for j in range((p - 1) // 2 + (p - 1) % 2)]
+    return lambda x, p: (list[x] if x < (p - 1) // 2 else  zeta - list[p-2-x])
+
+def generate_omega_for_l4(p):
+    omega = generate_omega_for_th2(p)
+    a = random.randint(1, 1000)
+    b = random.randint(0, 1000)
+    return lambda x, p: a * omega(x, p) + b
+
+def generate_bad_omega(p):
+    omega = generate_omega_for_th2(p)
+    print("I used to be ",[omega(x, p) for x in range(p - 1)])
+    return lambda x, p: omega(x, p) if x!= 0 else 800
+
+def generate_good_and_bad_omega(p):
+    omega = generate_omega_for_th2(p)
+    print("I used to be ",[omega(x, p) for x in range(p - 1)])
+    return (omega, lambda x, p: omega(x, p) if x!= 0 else 800)
